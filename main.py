4139a3dcad01422b298aa91966614cc216a67dbf
Kevin Wainwright <84409709+kevinw1982@users.noreply.github.com>
2025-01-15 18:52:37 +0000

    end of day 4

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 todos = []
 
 while True:
-    user_action = input("Type Add, Show or Exit: ")
+    user_action = input("Type Add, Show, Edit or Exit: ")
     user_action = user_action.strip()
+    user_action = user_action.capitalize()
     match user_action:
         case 'Add':
             todo = input("Enter a todo: ")
             todos.append(todo)
+        case 'Edit':
+            number = int(input("number of the todo to edit: "))
+            number = number - 1
+            todos[number]
+            new_todo = input("Enter new todo")
+            todos[number] = new_todo
         case 'Show':
             for item in todos:
                 print(item)
